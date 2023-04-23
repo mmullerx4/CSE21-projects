ddbc183bac1426c33d1f74d40d4714560910fe7a
mmullerx4 <mmullerx4@gmail.com>
2023-04-22 22:19:59 -0400

    update name

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string valueFromUser = Console.ReadLine();
+        int num = int.Parse(valueFromUser);
+
+        if (num >= 90)
+        {
+            Console.WriteLine("Congratulations, your grade is an A");
+        }
+        else if (num >= 80)
+        {
+            Console.WriteLine("Congratulations, your grade is a B");
+        }
+        else if (num >= 70)
+        {
+            Console.WriteLine("Congratulations, your grade is a c");
+        }
+        else if (num >= 60)
+        {
+            Console.WriteLine("Sorry, your grade is a D, you have not passed the class");
+        }
+        else
+        {
+            Console.WriteLine("Congratulations, your grade is a B");
+        }
     }
 }
