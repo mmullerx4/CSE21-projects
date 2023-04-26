0cc15b5a0de88e86bb8fafe0ed5d7841a076d02f
mmullerx4 <mmullerx4@gmail.com>
2023-04-26 11:07:57 -0400

    complete  3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,45 @@
+/* Melinda Muller
+Purpose: Practice C# with guess the magic number
+*/
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        //Console.WriteLine("Guess the Magic Number");
+
+            int guess = -1;
+
+            while (guess != magicNumber)
+            {    
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess < magicNumber)  
+                {
+                    Console.WriteLine("Guess higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Guess lower");
+                }    
+                else 
+                {
+                     Console.WriteLine("You guessed right!");
+                }
+                   
+                //guessCount += 1
+                
+            
+            //Console.WriteLine("Your total guess count was: {guessCount} ");
+
+            //playAgain = Console.Write("Would you like to play again (yes/no)? ");
+            
+            }        
     }
 }
