de9ecf2ecb2f2b675bdb5778fd2f7dc1e93c916d
mmullerx4 <mmullerx4@gmail.com>
2023-04-29 14:46:55 -0400

    Practicing 0428

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace Giraffe
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Sandbox World-Test!");
+        static void Main(string[] args)
+        {
+         string characterName = "John";
+         int characterAge;
+         characterAge = 35;
+         
+
+         Console.WriteLine("The once was a man named George " + characterName); //can concatenate
+         Console.WriteLine($"He was " + characterName + " years old");
+
+         characterName = "Mike"; //can change the name at anytime
+         Console.WriteLine("He liked the name {characterName}"); //can use this easier version
+         Console.WriteLine($"But he didn't like being {characterAge}");
+         
+         Console.ReadLine();
+        }
     }
 }
