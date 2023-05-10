614ee903d62231c93708090e498a004d22362c42
mmullerx4 <mmullerx4@gmail.com>
2023-05-10 13:12:53 -0400

    Wed am changes

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+              
+        int choice = -1; //choice needs to exist before using it
+        while (choice != 5)
+        { 
+            string menuOptions = "1)Write a new entry \n2)Display the journal \n3)Save the journal \n4)Load the journal \n5)Quit";
+            Console.WriteLine(menuOptions);
+            choice = Convert.ToInt16(Console.ReadLine()); //converting to int because user is responding to string 
+
+            if (choice == 1)
+            {
+                
+            }
+
+            else if (choice == 2)
+            {
+                
+            }
+
+            else if (choice == 3)
+            {
+                
+            }
+
+            else if (choice == 4)
+            {
+                
+            }
+
+            else
+            {
+                
+            }
+
+    }
     }
 }
