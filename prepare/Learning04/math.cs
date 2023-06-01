3d6fb7d243f820aa71c76b628bcee51ee1eeffa6
mmullerx4 <mmullerx4@gmail.com>
2023-06-01 13:26:19 -0400

    Learning04setup

--- prepare/Learning04/math.cs
+++ prepare/Learning04/math.cs
@@ -3,27 +3,28 @@ using System;
 class Math
 {
     //private attributes(variables)
-    private string name;
-    private string topic;
+    private string _textbookSection;
+    private string _problems;
 
 
-    //constructor
-    public Math(string name, string txtSection)
+    //constructor Note: constructors do not have a type. It is left empty. How many arguments do you need?
+    public Math(string problems, string textSection) //require 2 arguments: a value for each attribute in math class
     {
 
     }
 
     //method 1 return summary of assignment for student name and topic
-    public assignmentInstructions()
+
+    public string mathAssignment()
+    {
+        return mathAssignment; //returns assignment summary with student name and topic
+    }
+    public void GetHomeworkList() //display the homework list with section number and problems "Section 7.3 Problems 8-19".
     {
 
     }
 
-    public homeworkList()
-    {
-
-    }
-
+    
 
 
 
--- prepare/Learning04/writing.cs
+++ prepare/Learning04/writing.cs
@@ -1,30 +1,28 @@
+using System;
 class Writing
 {
     //private attributes(variables)
-    private string name;
-    private string topic;
+    private string name_title;
+    private string _topic;
 
 
     //constructor
-    public Writing(string name, string txtSection)
+    public Writing(string title, string topic) //requires a value for each attribute in this class (title, topic) ie. "European History, The Causes of World WarII".
     {
 
     }
 
     //method 1 return summary of assignment for student name and topic
-    public assignmentInstructions()
+
+    public void writingAssignment() //returns summary of assignment with student's name and topic
+    {
+        return writingAssignment;
+
+    }
+    public void GetWritingInformation() //get the writing information with title and student's name ie. "The Causes of World War II by Mary Waters".
     {
 
     }
-
-    public homeworkList()
-    {
-
-    }
-
-
-
-
 
 
 }
