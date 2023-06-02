8eecc314056028bbf2d5d77d83ca37de50baf7e6
mmullerx4 <mmullerx4@gmail.com>
2023-06-01 20:01:25 -0400

    Learning04.stage2

--- prepare/Learning04/math.cs
+++ prepare/Learning04/math.cs
@@ -1,27 +1,24 @@
 using System;
 
-class Math
+class Math : Assignments
 {
     //private attributes(variables)
-    private string _textbookSection;
+    private string _textBookSection;
     private string _problems;
 
 
-    //constructor Note: constructors do not have a type. It is left empty. How many arguments do you need?
-    public Math(string problems, string textSection) //require 2 arguments: a value for each attribute in math class
+    //constructor Note: constructors do not have a return type - It is left empty. How many arguments do you need?
+    public Math(string _problems, string _textBookSection, string _name, string _topic) : base(_name, _topic)//require 2 arguments: a value for each attribute in math class
     {
-
+        this._textBookSection = _textBookSection;
+        this._problems = _problems;
     }
 
-    //method 1 return summary of assignment for student name and topic
-
-    public string mathAssignment()
+    //method 1 return summary of assignment for student name and topic. Meth
+    //(Access Type) (Return Type) (Name( Parameters ))
+    public string GetHomeworkList() //display the homework list with section number and problems "Section 7.3 Problems 8-19".
     {
-        return mathAssignment; //returns assignment summary with student name and topic
-    }
-    public void GetHomeworkList() //display the homework list with section number and problems "Section 7.3 Problems 8-19".
-    {
-
+        return //returns assignment summary with student name and topic
     }
 
     
--- prepare/Learning04/writing.cs
+++ prepare/Learning04/writing.cs
@@ -1,5 +1,5 @@
 using System;
-class Writing
+class Writing : Assignments
 {
     //private attributes(variables)
     private string name_title;
@@ -7,21 +7,18 @@ class Writing
 
 
     //constructor
-    public Writing(string title, string topic) //requires a value for each attribute in this class (title, topic) ie. "European History, The Causes of World WarII".
+    public Writing(string _title, string _topic, string _name, string _topic) : base (_name, _topic)//requires a value for each attribute in this class (title, topic) ie. "European History, The Causes of World WarII".
     {
-
+        this.name_title = _title;
+        this
     }
 
     //method 1 return summary of assignment for student name and topic
 
-    public void writingAssignment() //returns summary of assignment with student's name and topic
-    {
-        return writingAssignment;
-
-    }
+   
     public void GetWritingInformation() //get the writing information with title and student's name ie. "The Causes of World War II by Mary Waters".
     {
-
+        return
     }
 
 
